84af5008b6c3c04d652f23c7ff165b66ac1ce73a
Kim Nguyen <kimnnguyen225@gmail.com>
2019-04-25 10:44:51 -0400

    create lib folder to store library jars, added commons-io-2.6.jar and use FileUtils.copyFile(sourceFile, targetFile);

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,13 +9,28 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Main {
-
+    /**
+     * @param args - collection of strings
+     */
     public static void main(String[] args) {
 
         // all of Paths's methods are static methods
-        Path sourceFile = Paths.get("Files", "RandomText.txt");
-        Path targetFile = Paths.get("Files", "target.txt");
+//        Path sourceFile = Paths.get("Files", "RandomText.txt");
+//        Path targetFile = Paths.get("Files", "target.txt");
 
+        File sourceFile = new File("Files/RandomText.txt");
+        File targetFile = new File("Files/copiedRandomText.txt");
+
+        /**
+         * intention action: Alt + Enter
+         */
+        try {
+            FileUtils.copyFile(sourceFile, targetFile);
+            System.out.println("File copied");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /**
         //replace_existing means that if the target file already exists, I want to delete it and
         // create a new version
         try {
@@ -21,6 +39,7 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
 
     }
 }
